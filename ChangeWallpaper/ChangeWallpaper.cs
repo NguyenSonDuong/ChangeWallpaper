ec76850fef804a42ee8edd10e4bdd58e3cbcec86
NguyenSonDuong <hunterdemon9x99@gmail.com>
2020-07-02 12:40:13 +0700

    ok

---
+++ ChangeWallpaper/ChangeWallpaper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeWallpaper
+{
+    class ChangeWallpaper
+    {
+        // đây là các cách sự dụng các hàm ccuar thư viện hệ thống. ở đây là User32.dll :3 
+        // cái hàm ngay dưới đây là để cài hình nền desktop
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int SystemParametersInfo(uint uiAction, uint uiParam, string pvParam, uint fWinIni);
+        // Hàm này thì để đăng ký Hotkey ( Phím tắt ế )
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        //hàm này để hủy đăng ký
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        // đây là enum chưa giá trị của các key 
+        public enum KeyModifier
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            WinKey = 8
+        }
+        // đây là key để set ảnh cho desktop ( :3 )
+        const uint SPI_SETDESKWALLPAPER = 0x14;
+        // đây là key cho việc cài băt file ( có lẽ vậy )
+        const uint SPIF_UPDATEINIFILE = 0x01;
+
+        // đây là hàm để gọi lại nè
+        public static void SetDesktopWallpaper(string path)
+        {
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE);
+        }
+    }
+}
---
+++ ChangeWallpaper/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangeWallpaper
+{
+    public partial class Form1 : Form
+    {
+        String anh1;
+        String anh2;
+        bool isChange;
+        public Form1()
+        {
+            InitializeComponent();
+            ChangeWallpaper.RegisterHotKey(this.Handle, 1999, (int)ChangeWallpaper.KeyModifier.Alt, Keys.F5.GetHashCode());
+            ChangeWallpaper.RegisterHotKey(this.Handle, 2000, (int)ChangeWallpaper.KeyModifier.Shift, Keys.F4.GetHashCode());
+            ChangeWallpaper.RegisterHotKey(this.Handle, 2001, (int)ChangeWallpaper.KeyModifier.Control, Keys.F4.GetHashCode());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if(open.ShowDialog() == DialogResult.OK)
+            {
+                
+                anh1 = open.FileName;
+                textBox1.Text = anh1;
+            }
+        }
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            // thi thoảng mk hay quên mấy giá trị này haha
+            // trong này là message trả về từ sự kiện nhấn phím tắt mà bạn đã đăng ký
+            // ví dụ như sau
+            // trong này bạn có thể làm mọi thứ với bài hôm nay thì mk sẽ đổi ảnh nền desktop
+            if(m.Msg == 0x0312)
+            {
+                int id = m.WParam.ToInt32();
+                if(id == 1999)
+                {
+                    if (isChange)
+                    {
+                        ChangeWallpaper.SetDesktopWallpaper(anh1);
+                        isChange = false;
+                    }
+                    else
+                    {
+                        ChangeWallpaper.SetDesktopWallpaper(anh2);
+                        isChange = true;
+                    }
+                }else if(id == 2000)
+                {
+                    Application.Exit();
+                }
+                else if(id == 2001)
+                {
+                    Show();
+                }
+                
+                
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if(open.ShowDialog() == DialogResult.OK)
+            {
+                anh2 = open.FileName;
+                textBox2.Text = anh2;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ChangeWallpaper.UnregisterHotKey(this.Handle, 1999);
+            ChangeWallpaper.UnregisterHotKey(this.Handle, 2000);
+        }
+        
+
+        private void đóngToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
